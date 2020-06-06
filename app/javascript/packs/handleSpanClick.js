313783254509c5fca5c5042fb118ae7818698670
neven <nevendrean@yahoo.fr>
2020-06-06 15:07:53 +0200

    refact

--- app/javascript/packs/handleSpanClick.js
+++ app/javascript/packs/handleSpanClick.js
@@ -5,9 +5,7 @@ function removeField() {
       e.target.parentNode.parentNode.parentNode.previousElementSibling
       // to prevent from removing every fieldset, we keep the first
     ) {
-      document
-        .getElementById(e.target.id)
-        .parentNode.parentNode.parentNode.remove();
+      e.target.parentNode.parentNode.parentNode.remove();
       // since we wrapped into several bootstrap divs
     }
   });
